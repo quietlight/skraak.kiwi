11d177fcc66f16f18b591d79ea766613a06580fe
David Cary <cdecary@gmail.com>
2023-09-26 10:04:13 +1300

    now it is updating properly

--- assets/index/pomona_why.jl
+++ assets/index/pomona_why.jl
@@ -1,3 +1,3 @@
 # This file was generated, do not modify it. # hide
 #hideall
-run(`d2 -t 104 --sketch --pad 100 --center /Volumes/SSD1/skraak.kiwi/_assets/scripts/pomona_why.d2 /Volumes/SSD1/skraak.kiwi/_assets/pomona_why.svg`)hy.svg`)
+run(`d2 -t 104 --sketch --pad 100 --center /Volumes/SSD1/skraak.kiwi/_assets/scripts/pomona_why.d2 /Volumes/SSD1/skraak.kiwi/_assets/pomona_why.svg`)
